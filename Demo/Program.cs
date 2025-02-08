fd0a5c4dfa21cd347d0be603d33cb39828bc5b49
amiraabdelazez <amiraabdelazez2@gmail.com>
2025-02-08 12:12:55 +0200

    02 Non-Generic Collection - Hashtable

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,10 +1,57 @@
-﻿namespace Demo
+﻿using System.Collections;
+
+namespace Demo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            #region 02 Non-Generic Collection - Hashtable
+           //Hashtable PhoneBook = new Hashtable();
+           //
+           //PhoneBook.Add("Ahmed", 111111);
+           //PhoneBook.Add("Ali", 2121);
+           //PhoneBook.Add("Mona", 11133);
+           //PhoneBook.Add("Marim", 111451);
+
+            //Console.WriteLine(PhoneBook.Count);
+            //Console.WriteLine(PhoneBook.IsFixedSize);
+            //Console.WriteLine(PhoneBook.IsReadOnly);
+
+            //foreach (var phone in PhoneBook.Keys)
+            //{
+            //    Console.WriteLine(phone);
+            //}
+
+            //foreach (var phone in PhoneBook.Values)
+            //{
+            //    Console.WriteLine(phone);
+            //}
+
+            //PhoneBook["ahmedness"] = 1234;
+
+            //Console.WriteLine(PhoneBook["Ahmed"]);
+
+            //PhoneBook.Clear();
+            //Console.WriteLine(PhoneBook.Count);
+
+            //PhoneBook.Clone();
+
+            ///var Flage = PhoneBook.Contains("Ahmed");
+            ///Console.WriteLine(Flage);
+            ///
+            ///Flage = PhoneBook.ContainsKey("Ahmed");
+            ///Console.WriteLine(Flage);
+            ///
+            ///
+            ///Console.WriteLine("====================");
+            ///
+            ///foreach (DictionaryEntry item in PhoneBook)
+            ///{
+            ///    Console.WriteLine(item);
+            ///} 
+            #endregion
+
         }
     }
 }
